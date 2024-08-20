559d3c6dbf65befe77db616732fb5b93e5b300a2
hussseinhaiadr6 <husseinhaidar670@gmail.com>
2024-08-20 13:01:55 +0200

    fix

---
+++ LP_Detection/DETR/DETR2.py
@@ -0,0 +1,110 @@
+import os
+import time
+import torch
+import torchvision.transforms as T
+
+from PIL import Image, ImageDraw, ImageFont
+# Define transforms
+transform = T.Compose([
+    T.Resize(800),
+    T.ToTensor(),
+    T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+
+
+# Function to convert bounding box format
+def box_cxcywh_to_xyxy(x):
+    x_c, y_c, w, h = x.unbind(1)
+    b = [(x_c - 0.5 * w), (y_c - 0.5 * h),
+         (x_c + 0.5 * w), (y_c + 0.5 * h)]
+    return torch.stack(b, dim=1)
+
+
+# Function to rescale bounding boxes
+def rescale_bboxes(out_bbox, size):
+    img_w, img_h = size
+    b = box_cxcywh_to_xyxy(out_bbox)
+    b = b * torch.tensor([img_w, img_h, img_w, img_h], dtype=torch.float32)
+    return b
+
+
+# Function to filter and process bounding boxes
+def filter_bboxes_from_outputs(outputs, threshold=0.7):
+    probas = outputs['pred_logits'].softmax(-1)[0, :, :-1]
+    keep = probas.max(-1).values > threshold
+    probas_to_keep = probas[keep]
+    bboxes_scaled = rescale_bboxes(outputs['pred_boxes'][0, keep], im.size)
+    return probas_to_keep, bboxes_scaled
+
+
+# Function to draw rectangles and save the image
+def draw_and_save_results(pil_img, prob=None, boxes=None, output_path=None):
+    draw = ImageDraw.Draw(pil_img)
+    colors = COLORS * 100
+    if prob is not None and boxes is not None:
+        for p, (xmin, ymin, xmax, ymax), c in zip(prob, boxes.tolist(), colors):
+            draw.rectangle([xmin, ymin, xmax, ymax], outline=tuple(int(x * 255) for x in c), width=3)
+            cl = p.argmax()
+            text = f'{finetuned_classes[cl]}: {p[cl]:0.4f}'
+            font = ImageFont.truetype("arial.ttf", 24)
+            draw.text((xmin, ymin), text,font=font, fill="red")
+    if output_path:
+        pil_img.save(output_path)
+
+
+# Function to run workflow on an image
+def run_workflow(my_image, my_model, threshold=0.7):
+    img = transform(my_image).unsqueeze(0)
+    outputs = my_model(img)
+    probas_to_keep, bboxes_scaled = filter_bboxes_from_outputs(outputs, threshold=threshold)
+    return probas_to_keep, bboxes_scaled
+
+
+# Define model and load weights
+model = torch.hub.load('facebookresearch/detr',
+                       'detr_resnet50',
+                       pretrained=False,
+                       num_classes=4)
+
+checkpoint = torch.load('./DETR_weights/checkpoint_drive.pth',
+                        map_location='cpu')
+
+model.load_state_dict(checkpoint['model'],
+                      strict=False)
+
+
+model.eval()
+
+# Define classes and colors
+finetuned_classes = ['LP', 'Background', 'English_plate', 'Iraq_plate']
+COLORS = [[0.000, 0.447, 0.741], [0.850, 0.325, 0.098], [0.929, 0.694, 0.125], [0.494, 0.184, 0.556]]
+
+# Directory containing images
+img_dir = 'C:/Users/HHR6/PycharmProjects/Testing_Pipeline/Acksession-Project/yolov8_compiled_dataset/test/images/'
+
+# Directory to save images with rectangles
+output_dir = 'C:/Users/HHR6/PycharmProjects/Testing_Pipeline/results_detr'
+
+# Ensure output directory exists, create it if necessary
+os.makedirs(output_dir, exist_ok=True)
+
+# Loop over each image in the directory
+for img_name in os.listdir(img_dir):
+    if img_name.endswith('.jpg') or img_name.endswith('.png'):
+        img_path = os.path.join(img_dir, img_name)
+        im = Image.open(img_path)
+
+        # Measure inference time
+        start_time = time.time()
+
+        # Run workflow on the image
+        probas_to_keep, bboxes_scaled = run_workflow(im, model, threshold=0.4)
+
+
+        # Measure inference time
+        inference_time = time.time() - start_time
+        print(f"Inference time for {img_name}: {inference_time:.2f} seconds")
+
+        # Draw rectangles and save the image
+        output_path = os.path.join(output_dir, f'result_{img_name}')
+        draw_and_save_results(im, probas_to_keep, bboxes_scaled, output_path)
---
+++ LP_Detection/Yolo/inference.py
@@ -0,0 +1,102 @@
+import os
+from PIL import Image
+import torch
+
+# Load your custom model
+model = torch.hub.load(r'C:\Users\HHR6\PycharmProjects\Testing_Pipeline\yolov5', 'custom',force_r0load=True, path='./Models/IranLPDetection3.pt',source="local")
+model.conf = 0.45  # NMS confidence threshold
+iou = 0.45  # NMS IoU threshold
+model.agnostic = True
+
+# Load the second model (assuming a second model is required)
+second_model = torch.hub.load(r'C:\Users\HHR6\PycharmProjects\Testing_Pipeline\yolov5', 'custom',force_reload=True, path='./Models/IRAN_Iraq.pt',source="local")
+second_model.conf = 0.45
+second_model.agnostic = True
+
+# Directory paths
+input_dir = './processed_iran'
+output_dir = './output9'
+old_iraq_dir = os.path.join(output_dir, 'OLD_Iraq')
+new_iraq_dir = os.path.join(output_dir, 'New_Iraq')
+iran_dir = os.path.join(output_dir, 'Iran')
+
+# Create necessary directories if they don't exist
+os.makedirs(old_iraq_dir, exist_ok=True)
+os.makedirs(new_iraq_dir,exist_ok=True)
+os.makedirs(iran_dir, exist_ok=True)
+
+# Function to resize image
+def resize_image(input_path, size=(640, 640)):
+    with Image.open(input_path) as img:
+        resized_img = img.resize(size, Image.LANCZOS)
+        return resized_img
+
+# Function to extend the bounding box
+def extend_bbox(bbox, image_size, factor=4):
+    x1, y1, x2, y2 = bbox
+    width, height = image_size
+
+    w = x2 - x1
+    h = y2 - y1
+
+    x1_new = max(x1 - w * (factor - 1) // 2, 0)
+    y1_new = max(y1 - h * (factor - 1) // 2, 0)
+    x2_new = min(x2 + w * (factor - 1) // 2, width)
+    y2_new = min(y2 + h * (factor - 1) // 2, height)
+
+    return (x1_new, y1_new, x2_new, y2_new)
+
+# Function for inference
+def inference_image(input_path):
+    resized_image = resize_image(input_path)
+    results = model(resized_image)
+    df = results.pandas().xyxy[0]
+    print(df)
+    for index, row in df.iterrows():
+        class_id = int(row['class'])
+        bbox = [int(row['xmin']), int(row['ymin']), int(row['xmax']), int(row['ymax'])]
+
+        if class_id == 0 or class_id==1:  # Class ID 2 for extending the bounding box
+            # Extend the bounding box by a factor of 4
+            extended_bbox = extend_bbox(bbox, resized_image.size, factor=4)
+
+            # Crop the extended image
+            extended_cropped_image = resized_image.crop(extended_bbox)
+
+            # Save the extended cropped image (optional)
+
+
+
+            # Use the second model for further processing
+            second_results = second_model(extended_cropped_image)
+            second_df = second_results.pandas().xyxy[0]
+
+            # Print the results of the second model
+
+            for index, row in second_df.iterrows():
+                class_id = int(row['class'])
+                bbox = [int(row['xmin']), int(row['ymin']), int(row['xmax']), int(row['ymax'])]
+                if class_id == 0:
+                    cropped_image=extended_cropped_image.crop(bbox)
+                    save_path=os.path.join(new_iraq_dir,os.path.basename(input_path))
+                    cropped_image.save(save_path)
+                else:
+                    cropped_image = extended_cropped_image.crop(bbox)
+                    save_path = os.path.join(iran_dir, os.path.basename(input_path))
+                    cropped_image.save(save_path)
+
+        else:
+            # Save the bounding box image if it's not class 2
+            cropped_image = resized_image.crop(bbox)
+            save_path = os.path.join(old_iraq_dir, os.path.basename(input_path))
+            cropped_image.save(save_path)
+
+
+def inference_directory(input_dir):
+    paths=os.listdir(input_dir)
+    for path in paths:
+        inference_image(os.path.join(input_dir,path))
+
+# Test the function
+inference_image(r"C:\Users\HHR6\PycharmProjects\Testing_Pipeline\Acksession-Project\yolov8_compiled_dataset\test\images\1_jpg.rf.e0d71bfac749526f7f57a7274f7f8f1e.jpg")
+inference_directory(r"C:\Users\HHR6\PycharmProjects\Testing_Pipeline\car_img-validation/images")
